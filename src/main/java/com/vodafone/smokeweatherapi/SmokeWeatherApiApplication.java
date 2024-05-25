8f23207e55b6df186bb8375f51836aca15b02b33
danij <danijlg92@uma.es>
2024-05-25 21:44:10 +0200

    created project structure

---
+++ src/main/java/com/vodafone/smokeweatherapi/SmokeWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.vodafone.smokeweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokeWeatherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmokeWeatherApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vodafone/smokeweatherapi/SmokeWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodafone.smokeweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeWeatherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
